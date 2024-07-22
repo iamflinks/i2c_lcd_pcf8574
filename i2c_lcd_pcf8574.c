ed222e913178416dea9a226a69d4db5a1c83b65b
Femi Olugbon <olugbonfemi@gmail.com>
2024-07-22 03:55:57 -0400

    driver folder creation

---
+++ i2c_lcd_pcf8574.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "i2c_lcd_pcf8574.h"
+#include "esp_log.h"
+#include "esp_check.h"
+#include "driver/i2c.h"
+
+
+
---
+++ include/i2c_lcd_pcf8574.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifndef I2C_LCD_PCF8574_H
+#define I2C_LCD_PCF8574_H
+
+#include "esp_err.h"
+#include "driver/i2c.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif // C++ extern
+
+#endif // I2C_LCD_PCF8574_H
